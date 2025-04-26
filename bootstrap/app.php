b4a34b7d8f594fd4b0d543b683ad89ed4da1c8af
daniel09pi <dev@danielsell.com>
2025-04-26 11:11:05 +0200

    inertia  vue3 und vite eingerichtet!

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Http\Middleware\HandleInertiaRequests;
+
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,6 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
---
+++ resources/js/Pages/home.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>{{ message }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello, Vue 3 with Composition API!');
+</script>
